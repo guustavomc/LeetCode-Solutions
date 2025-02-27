4c224af40dd0f589f7046c82ccb22152673472d1
guustavomc <49313519+guustavomc@users.noreply.github.com>
2025-02-26 22:25:28 -0300

    Create Solution.java

---
+++ Climbing Stairs/Solution.java
@@ -0,0 +1,51 @@
+/*You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45*/
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        else if(n==2){
+            return 2;
+        }
+        else{
+            return recursiveSteps(1,1,2,2,n);
+        }
+    }
+
+    public int recursiveSteps(int nSteps1, int nStepsTaken1, int nSteps2, int nStepsTaken2, int target){
+        int nNextStep = nSteps2+1;
+        int nNextStepTaken = nStepsTaken1+nStepsTaken2;
+
+        if(nNextStep==target){
+            return nStepsTaken1+nStepsTaken2;
+        }
+        else{
+            return recursiveSteps(nSteps2,nStepsTaken2,nNextStep,nNextStepTaken,target);
+        }
+    }
+}
